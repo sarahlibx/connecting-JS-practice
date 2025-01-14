f80a49a1432ef8762c1319a2f0237e834d0446aa
Sarah Smith <155038730+sarahlibx@users.noreply.github.com>
2025-01-14 16:34:53 +0000

    Additional practice

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-const mainTitle = document.querySelector("H1");
+/* const mainTitle = document.querySelector("H1");
 mainTitle.innerText = "Yay, Javascript!";
 
 //console.log(mainTitle.innerText);
 
 const body = document.querySelector("body");
 body.style.backgroundColor = "#FEF2F1";
-console.log(body);
+console.log(body); */
+
+const firstListItem = document.querySelector("ul li");
+console.log(firstListItem);
