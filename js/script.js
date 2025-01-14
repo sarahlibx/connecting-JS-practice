fa344ab74efa6824a63e31c2a681305370752584
Sarah Smith <155038730+sarahlibx@users.noreply.github.com>
2025-01-14 11:30:24 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const mainTitle = document.querySelector("H1");
+mainTitle.innerText = "Yay, Javascript!";
+
+//console.log(mainTitle.innerText);
+
+const body = document.querySelector("body");
+body.style.backgroundColor = "#FEF2F1";
+console.log(body);
